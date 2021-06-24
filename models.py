b60bd8ee872cf2dfe4dbc76a584041bad2031a96
Marc Blackmer <marc.blackmer@1nterrupt.com>
2021-06-23 22:14:37 -0400

    Revert "Change URI variable name to match Heroku variable name"
    That didn't work
    
    This reverts commit 8748cc961df3b56eea3cad16c1339de9f2766a24.

--- models.py
+++ models.py
@@ -17,7 +17,7 @@ db = SQLAlchemy()
 
 def setup_db(app, database_path=DB_PATH):
     try:
-        app.config['DATABASE_URL'] = database_path
+        app.config['SQLALCHEMY_DATABASE_URI'] = database_path
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         db.app = app
         db.init_app(app)
